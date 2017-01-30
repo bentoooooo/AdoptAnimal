f35b59d1ea212f1f2aa856214976d97dac4c64d2
bentoooooo <bentoooooo@gmail.com>
2017-01-30 18:18:01 +0000

    Ponto 6

--- app/src/main/java/pt/ipbeja/adoptanimal/ListAnimal.java
+++ app/src/main/java/pt/ipbeja/adoptanimal/ListAnimal.java
@@ -3,15 +3,17 @@ package pt.ipbeja.adoptanimal;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ListAnimal extends Fragment {
+public class ListAnimal extends ListFragment {
 
 
     public ListAnimal() {
@@ -23,7 +25,11 @@ public class ListAnimal extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_animal, container, false);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1, animals.animal);
+
+        setListAdapter(adapter);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
 }
