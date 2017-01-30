e4d2d59b0af5ccec2286742e85601f5d9318f596
bentoooooo <bentoooooo@gmail.com>
2017-01-30 17:37:15 +0000

    Ponto 5

--- app/src/main/java/pt/ipbeja/adoptanimal/MainActivity.java
+++ app/src/main/java/pt/ipbeja/adoptanimal/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+                Toast.makeText(this, animals.animal[0], Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, animals.animalContent[0], Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, animals.animal[1], Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, animals.animalContent[1], Toast.LENGTH_SHORT).show();
                 Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
         if (findViewById(R.id.fragment_container) != null) {
             if (savedInstanceState != null) {
---
+++ app/src/main/java/pt/ipbeja/adoptanimal/animals.java
@@ -0,0 +1,16 @@
+package pt.ipbeja.adoptanimal;
+
+/**
+ * Created by Bento on 30/01/2017.
+ */
+
+public class animals {
+
+    static String[] animal = {
+            "AnimalOne",
+            "AnimalTwo"
+    };
+    static String[] animalContent = {
+            "DogOne",
+            "CatOne"};
+}
