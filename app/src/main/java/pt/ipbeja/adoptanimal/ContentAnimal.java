b60cae60d9cb8274aeed50dcfcb3081cd1cfb62a
bentoooooo <bentoooooo@gmail.com>
2017-01-31 16:34:51 +0000

    Ponto 10

--- app/src/main/java/pt/ipbeja/adoptanimal/ContentAnimal.java
+++ app/src/main/java/pt/ipbeja/adoptanimal/ContentAnimal.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -24,5 +25,25 @@ public class ContentAnimal extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_content_animal, container, false);
+    }
+
+    @Override public void onStart() {
+        super.onStart();
+        // During startup, check if there are arguments passed to the fragment.
+        // onStart is a good place to do this because the layout has already been
+        // applied to the fragment at this point so we can safely call the method
+        // below that sets the article text.
+
+        Bundle args = getArguments();
+        if (args != null) {
+            // Set article based on argument passed in
+            int currentPosition = args.getInt("position");
+            TextView article = (TextView) getActivity().findViewById(R.id.txtContent);
+            article.setText(animals.animalContent[currentPosition]);
+        }
+        else{
+            TextView article = (TextView) getActivity().findViewById(R.id.txtContent);
+            article.setText(animals.animalContent[0]);
+        }
     }
 }
